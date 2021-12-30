c1135310371287764290c221b2c8b8115841dbba
Doaa Albishri <Duaa_ali@outlook.sa>
2021-12-30 11:52:04 +0000

    Section 2 - Login / Registration

--- Messenger App/Controllers/ConversationViewController.swift
+++ Messenger App/Controllers/ConversationViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ConversationViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+       // let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+       // if !isLoggedIn {
+            // present login view controller
+            //let vc = LoginViewController()
+            //self.navigationController?.pushViewController(vc, animated: true)
+            //            let vc = LoginViewController()
+            //            let nav = UINavigationController(rootViewController: vc)
+            //            nav.modalPresentationStyle = .fullScreen
+            //            present(nav, animated: false)
+      //  }
+    }
 }
+
 
