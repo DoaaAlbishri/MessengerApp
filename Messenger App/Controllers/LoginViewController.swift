0108aba65e50162237eb335076ff4d7fd259e76f
doaa <duaa_ali@outlook.sa>
2022-01-04 06:57:37 +0000

    Section 4 - Firebase Setup and Login

--- Messenger App/Controllers/LoginViewController.swift
+++ Messenger App/Controllers/LoginViewController.swift
@@ -6,24 +6,42 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class LoginViewController: UIViewController {
 
+    
+    @IBOutlet weak var email: UITextField!
+    
+
+    @IBOutlet weak var password: UITextField!
+    
+
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        guard let email = email.text,
+              let password = password.text,
+              !email.isEmpty,
+              !password.isEmpty
+        else{
+        //toasts
+            return
+        }
+        // Firebase Login
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { authResult, error in
+            guard let result = authResult, error == nil else {
+                print("Failed to log in user with email \(email)")
+                return
+            }
+            let user = result.user
+            print("logged in user: \(user)")
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Messenger App/Controllers/RegisterViewController.swift
+++ Messenger App/Controllers/RegisterViewController.swift
@@ -6,9 +6,20 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var fName: UITextField!
+    
+    @IBOutlet weak var lName: UITextField!
+    
+    
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
     
     @IBOutlet weak var imageView: UIImageView!
     
@@ -19,12 +30,38 @@ class RegisterViewController: UIViewController {
         let tapGR = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped))
                     imageView.addGestureRecognizer(tapGR)
                     imageView.isUserInteractionEnabled = true
+       // imageView.layer.borderWidth = 2
     }
 
     @objc func imageTapped(sender: UITapGestureRecognizer) {
     presentPhotoActionSheet()
     }
     
+    
+    @IBAction func registerButton(_ sender: UIButton) {
+        guard let firstName = fName.text ,
+              let lastName = lName.text ,
+              let email = email.text,
+              let password = password.text,
+              !firstName.isEmpty ,
+              !lastName.isEmpty,
+              !email.isEmpty,
+              !password.isEmpty
+        else{
+        //toasts
+            return
+        }
+        // Firebase Login / check to see if email is taken
+        // try to create an account
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { authResult , error  in
+            guard let result = authResult, error == nil else {
+                print("Error creating user")
+                return
+            }
+            let user = result.user
+            print("Created User: \(user)")
+        })
+    }
     
 }
 
