4ca2058c6f53a63412d7819c661d35bafa50eacf
doaa <duaa_ali@outlook.sa>
2022-01-06 12:27:26 +0000

    profile image

--- Messenger App/Controllers/ProfileViewController.swift
+++ Messenger App/Controllers/ProfileViewController.swift
@@ -8,8 +8,14 @@
 import UIKit
 import FirebaseAuth
 import FirebaseStorage
+import SDWebImage
+
 class ProfileViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
+    
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var profileImage: UIImageView!
+    
     
     let data = ["Log Out"]
     override func viewDidLoad() {
@@ -17,15 +23,41 @@ class ProfileViewController: UIViewController {
           //tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
           tableView.delegate = self
           tableView.dataSource = self
-//        tableView.tableHeaderView = createTableHeader()
+          tableView.tableHeaderView = createTableHeader()
+          profileImage.layer.cornerRadius = profileImage.frame.width/2
       }
-//    func createTableHeader() -> UIView? {
-//           guard let email = UserDefaults.standard.value(forKey: "email") as? String else {
-//               return nil
-//           }
-//        //let view = UIView()
-//        rerturn nil
-//  }
+    func createTableHeader() -> UIView? {
+           guard let email = UserDefaults.standard.value(forKey: "email") as? String else {
+               return nil
+           }
+        
+        let safeEmail = DatabaseManger.safeEmail(emailAddress: email)
+               let filename = safeEmail + "_profile_picture.png"
+               let path = "images/"+filename
+        StorageManager.shared.downloadURL(for: path, completion: {[weak self ] result in
+                  switch result {
+                  case .success(let url):
+                   //   self.profileImage.sd_setImage(with: url, completed: nil)
+                      self?.downloadImage(imageView: (self?.profileImage)!, url: url)
+                  case .failure(let error):
+                      print("Failed to get download url: \(error)")
+                  }
+              })
+        return headerView
+  }
+    func downloadImage(imageView : UIImageView , url : URL){
+        URLSession.shared.dataTask(with: url , completionHandler: {
+            data , _ , error in
+            guard let data = data , error == nil else{
+                return
+            }
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                print("success")
+                imageView.image = image
+            }
+        }).resume()
+    }
 }
   extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
